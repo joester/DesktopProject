6bab1983e13c7d94bc3079ca3b361a8ad952a21e
Leo Lin <Leo Lin@169.234.104.5>
2013-01-18 00:08:00 -0800

    init

---
+++ DesktopProject/src/com/vgdc/team4/Game.java
@@ -0,0 +1,51 @@
+package com.vgdc.team4;
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
+public class Game implements ApplicationListener{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new LwjglApplication(new Game());
+
+	}
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
