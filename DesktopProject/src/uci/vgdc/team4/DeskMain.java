5485f5c43e3c164256296f1320249d7d7f587e35
Leo Lin <Leo Lin@192.168.112.120>
2013-01-20 17:59:25 -0800

    unable to resize now

--- DesktopProject/src/uci/vgdc/team4/DeskMain.java
+++ DesktopProject/src/uci/vgdc/team4/DeskMain.java
@@ -1,9 +1,15 @@
 package uci.vgdc.team4;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class DeskMain {
 	public static void main(String[] args) {
-		new LwjglApplication(new Game(), "dog", 512, 512, true);
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.resizable = false;
+		config.height = 800;
+		config.width = 1280;
+		config.useGL20 = true;
+		new LwjglApplication(new Game(), config);
 	}
 }
